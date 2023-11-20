0d8252de27c75ca4f32b52b3634a1cf6becf7d76
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2023-11-20 10:14:56 -0600

    clopud_deploy code

--- app1.py
+++ app1.py
@@ -1,45 +1,52 @@
 from flask import Flask, render_template, request, jsonify
+from google.cloud import storage
 from PyPDF2 import PdfReader
 import openai
 
 app = Flask(__name__)
-summaries = {} 
+summaries = {}
 
+# Initialize Google Cloud Storage client
+storage_client = storage.Client()
+bucket_name = 'your_bucket_name'  # Replace with your GCS bucket name
 
 openai.api_key = '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'
-MAX_TOKENS = 4000  
+MAX_TOKENS = 4000
 
 @app.route('/')
 def index():
     return render_template('index1.html')
 
-@app.route('/api/summarize_text', methods=['POST'])
-def summarize_text():
-    if request.method == 'POST':
-        data = request.get_json()
-        text = data.get('text')
-        if not text:
-            return jsonify({'error': 'No text found'})
-        summary = generate_summary(text)
-        summary_id = len(summaries) + 1
-        summaries[summary_id] = summary
-        return jsonify({'summary_id': summary_id})
-
-@app.route('/api/summarize_file', methods=['POST'])
-def summarize_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file found'})
-        
+@app.route('/api/upload_and_summarize', methods=['POST'])
+def upload_and_summarize():
+    if 'file' in request.files:
         pdf_file = request.files['file']
-        content = extract_text_from_pdf(pdf_file)
-        if not content:
-            return jsonify({'error': 'Failed to extract text from the PDF'})
-        
-        summary = generate_summary(content)
-        summary_id = len(summaries) + 1
-        summaries[summary_id] = summary
-        return jsonify({'summary_id': summary_id})
+        if pdf_file.filename != '':
+            # Upload file to Google Cloud Storage
+            blob = storage_client.bucket(bucket_name).blob(pdf_file.filename)
+            blob.upload_from_file(pdf_file)
+
+            # Extract text from the uploaded PDF file
+            content = extract_text_from_pdf(pdf_file)
+
+            # Generate summary from the extracted text
+            summary = generate_summary(content)
+            summary_id = len(summaries) + 1
+            summaries[summary_id] = summary
+
+            return jsonify({'summary_id': summary_id})
+    
+    elif 'text' in request.json:
+        text = request.json['text']
+        if text:
+            # Generate summary from the input text
+            summary = generate_summary(text)
+            summary_id = len(summaries) + 1
+            summaries[summary_id] = summary
+
+            return jsonify({'summary_id': summary_id})
+    
+    return jsonify({'error': 'Invalid input'})
 
 @app.route('/summary/<int:summary_id>')
 def show_summary(summary_id):
@@ -58,7 +65,6 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def generate_summary(content):
-    # Split content into chunks of maximum token length
     chunks = [content[i:i + MAX_TOKENS] for i in range(0, len(content), MAX_TOKENS)]
     summaries = []
     for chunk in chunks:
